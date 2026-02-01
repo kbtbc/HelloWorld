6754d662f2116263c0119572f0775679435b1bd9
Sandbox user <tools@notanumber.com>
2026-02-01 19:02:38 +0000

    I want a super simple empty html landing page that just says 'hello world'.

---
+++ webapp/src/pages/Index.tsx
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white">
+      <h1 className="text-4xl font-bold">hello world</h1>
+    </div>
+  );
+};
+
+export default Index;
