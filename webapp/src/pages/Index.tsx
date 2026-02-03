4fb14b677de21f633a32d3ae8c9eba3699daa25d
Sandbox user <tools@notanumber.com>
2026-02-03 11:59:18 +0000

    make the text blue.

--- webapp/src/pages/Index.tsx
+++ webapp/src/pages/Index.tsx
@@ -1,7 +1,7 @@
 const Index = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-white">
-      <h1 className="text-4xl font-bold">hello world</h1>
+      <h1 className="text-4xl font-bold text-blue-600">hello world</h1>
     </div>
   );
 };
